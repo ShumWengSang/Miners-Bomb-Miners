4e7d77d030da84b8048425a7cea6633a6dcfabc7
Roland <ballwallcallall@gmail.com>
2015-11-18 09:07:10 +0800

    singleplayer init commit

---
+++ Miners Kill Miners/Assets/Scripts/GameSceneController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Roland
+{
+    public class GameSceneController : MonoBehaviour
+    {
+
+        ChangeScenes changeScene = null;
+
+        // Use this for initialization
+        void Start()
+        {
+            changeScene = GetGlobalObject.FindAndGetComponent<ChangeScenes>(this.gameObject, "Global");
+
+            //Check whether its sandbox or multiplayer.
+            //If it is single player, generate the default map.
+            //else, generate data and send over and generate again after taking.
+
+
+        }
+
+        public void ChangeScene(string newScene)
+        {
+            changeScene.LoadScene(newScene);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
