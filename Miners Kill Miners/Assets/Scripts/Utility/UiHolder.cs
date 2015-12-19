f4d366921ca80f23528144aaec1574c7c038c34f
Roland <ballwallcallall@gmail.com>
2015-12-20 01:40:18 +0800

    add UiHolder

---
+++ Miners Kill Miners/Assets/Scripts/Utility/UiHolder.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class UiHolder : MonoBehaviour {
+
+    public Text AmountOfBombs;
+    public Text HP;
+    public Image TypeOfBomb;
+}
