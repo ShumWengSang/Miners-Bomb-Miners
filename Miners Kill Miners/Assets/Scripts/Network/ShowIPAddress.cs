dc122e5fe1ddce02b5ef4477e89e25e826622843
Roland <ballwallcallall@gmail.com>
2015-12-20 14:55:16 +0800

    add meta

---
+++ Miners Kill Miners/Assets/Scripts/Network/ShowIPAddress.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.UI;
+public class ShowIPAddress : MonoBehaviour
+{
+    public Text ui;
+
+    void Start()
+    {
+        ui.text = getIP();
+    }
+
+    static string getIP()
+    {
+        IPHostEntry host;
+        string localIP = "?";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+            }
+        }
+        return localIP;
+    }
+}
