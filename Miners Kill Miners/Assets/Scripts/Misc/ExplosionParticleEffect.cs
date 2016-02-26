6d61683e70d3143be405ee23c264efeff6a7ff8e
Roland <ballwallcallall@gmail.com>
2016-02-26 20:02:22 +0800

    Misc/ExplosionParticleEffect.cs

---
+++ Miners Kill Miners/Assets/Scripts/Misc/ExplosionParticleEffect.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionParticleEffect : Singleton<ExplosionParticleEffect>
+{
+    protected ExplosionParticleEffect()
+    {
+    }
+
+    void Start()
+    {
+        ParentParticle = GameObject.Find("ParticleParent").transform;
+        ParentParticle.gameObject.SetActive(false);
+        Child = ParentParticle.GetChild(0).GetComponent<ParticleSystem>() ;
+    }
+    Transform ParentParticle;
+    ParticleSystem Child;
+
+    public void PositionParticleAndExplode( Transform HealthToExplode)
+    {
+        Vector3 ResultPos = Camera.main.ScreenToWorldPoint(HealthToExplode.position);
+        ParentParticle.gameObject.SetActive(false);
+        ParentParticle.position = new Vector3(ResultPos.x, ResultPos.y, ParentParticle.position.z);
+        ParentParticle.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if(!Child.isPlaying)
+        {
+            ParentParticle.gameObject.SetActive(false);
+        }
+    }
+}
